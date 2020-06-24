7c597f6b92e09a1bae37e8122ef4f454de524499
Andres Ruiz <andrsruiz@gmail.com>
2020-06-24 13:42:22 -0500

    Add CurrentWeather Component

---
+++ src/components/CurrentWeather.vue
@@ -0,0 +1,28 @@
+<template>
+<div class="weather">
+	<div class="weather-top">
+
+	</div>
+	<div class="weather-bottom">
+
+	</div>
+</div>
+</template>
+
+<script>
+</script>
+
+<style scoped>
+.weather {
+	display: flex;
+
+}
+
+.weather-top {
+
+}
+
+.weather-bottom {
+
+}
+</style>
